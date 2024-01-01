1e034656c537cd48c9cdf32b3a41f688abbd61fc
nikoagge <nikoagge@gmail.com>
2024-01-01 09:54:24 +0200

    Initial Commit

---
+++ HappyNewYearCardInSwiftUI/HappyNewYearCardInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyNewYearCardInSwiftUIApp.swift
+//  HappyNewYearCardInSwiftUI
+//
+//  Created by Nikos Aggelidis on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HappyNewYearCardInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
